cc7febd1a7e792f44d043640d44635a4cfe4a1ee
Yazan-Dev9 <yksy.dev@gmail.com>
2025-04-14 23:36:51 +0300

    Add App Bar

--- src/smp/gui/main.py
+++ src/smp/gui/main.py
@@ -5,13 +5,36 @@ def main(page:Page):
     # page.window.width = page.width
     # page.window.height = 730
     page.bgcolor = Colors.CYAN_900
-    page.theme_mode = ThemeMode.DARK
-
+    page.theme_mode = ThemeMode.SYSTEM
     page.auto_scroll = True
 
-    page.add(
-        Text("Hello",bgcolor=Colors.WHITE,color=Colors.BLACK,text_align=TextAlign.CENTER,width=page.width,weight=FontWeight.BOLD)
+    page.appbar = AppBar(
+        leading=Icon(icons.HOME),
+        leading_width=40,
+        title=Text("SMP"),
+        center_title=True,
+        bgcolor=Colors.CYAN_900,
+        actions=[
+            IconButton(icons.SEARCH, tooltip="Search"),
+            IconButton(icons.PERSON,tooltip="Person"),
+            IconButton(icons.NOTIFICATIONS, tooltip="Notfications"),
+            IconButton(icons.SETTINGS, tooltip="Settings"),
+            PopupMenuButton(
+                items=[
+                    PopupMenuItem(text="Students Manager"),
+                    PopupMenuItem(text="Teacher Manager"),
+                    PopupMenuItem(text="Class Manager"),
+                    PopupMenuItem(text="Subjects Manager"),
+                    PopupMenuItem(),
+                    PopupMenuItem(text="Crit"),
+                    PopupMenuItem(text="Abuts"),
+                ],
+            )
+        ]
     )
+            
+
+    page.add()
 
     page.update()
 
